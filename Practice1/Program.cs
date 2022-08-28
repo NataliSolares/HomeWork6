2bce1bf1734fe623e910af24bbc6bd90d0ea6a74
Natali <n_sunberry@inbox.ru>
2022-08-28 21:49:01 +0300

    Задачи

---
+++ Practice1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+double[,] mass = new double[3,4];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i,j]=new Random().NextDouble(); 
+        Console.Write("{0,6:F2}",mass[i,j]);
+    }
+Console.WriteLine();
+}

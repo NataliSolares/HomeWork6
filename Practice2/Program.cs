365e752002354bdb98ab10321300d5175314a24d
Natali <n_sunberry@inbox.ru>
2022-08-30 22:06:22 +0300

    Задачи

---
+++ Practice2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int[,] mass = new int[3, 4];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i,j] = i+j;
+        Console.Write($" {mass[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
---
+++ Practice3/Program.cs
@@ -0,0 +1,24 @@
+﻿// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+int[,] mass = new int[5, 5];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(5, 5);
+        Console.Write($" {mass[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int n = 0; n < mass.GetLength(0); n++)
+{
+    for (int m = 0; m < mass.GetLength(1); m++)
+    {
+        if (n % 2 == 0 && m % 2 == 0)
+        {
+            mass[n, m] = mass[n, m] * mass[n, m];
+        }
+        Console.Write($" {mass[n, m]} ");
+    }
+    Console.WriteLine();
+}
